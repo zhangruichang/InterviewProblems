635293302c2d63f98789c427068c649ac6435996
zhangruichang <zhangruichang112@gmail.com>
2015-03-31 00:39:10 +0800

    LCA

---
+++ LCA/main.cpp
@@ -0,0 +1,79 @@
+/*
+Author: richard
+Contact: zhangruichang112@gmail.com
+*/
+#include<set>
+#include<map>
+#include<list>
+#include<cmath>
+#include<queue>
+#include<stack>
+#include<ctime>
+#include<cstdio>
+#include<string>
+#include<vector>
+#include<climits>
+#include<cstdlib>
+#include<cstring>
+#include<fstream>
+#include<iostream>
+#include<algorithm>
+#include <unordered_set>
+#include <unordered_map>
+using namespace std;
+const int maxn = 1e6 + 10;
+typedef long long LL;
+typedef unsigned long long ULL;
+//int, -2^31~2^31-1    -2.1*10^9~2.1*10^9 (-2147483648-2147483647)
+//unsigned int, 0~2^32-1  0~4.2*10^9
+//long long  -2^63~2^63-1 -9*10^18~9*10^18
+//unsigned long long 0~2^64-1  0~1.8*10^19
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+#define fi first
+#define se second
+int getint(){
+	int t = 0, flag = 1; char c = getchar();
+	while (c<'0' || c>'9' || c == '-')
+	{
+		if (c == '-')
+			flag = -1;
+		c = getchar();
+	}
+	while (c >= '0'&&c <= '9')
+		t = t * 10 + c - '0', c = getchar();
+	return t*flag;
+}
+int GCD(int m, int n)
+{
+    if(!m) return n;
+    return GCD(n%m, m);//yushu and chushu
+}
+int a[maxn], n, t, m;
+struct Node
+{
+    int val;
+    Node* left, *right;
+};
+Node* LCA(Node*root, Node* p1, Node*p2)
+{// p1&&p2, p1!=p2, p1 p2 in tree
+    if(!root) return NULL;
+    if(root==p1 || root==p2) return root;
+    Node* leftLCA=LCA(root->left, p1, p2);
+    Node* rightLCA=LCA(root->right, p1, p2);
+    if(leftLCA && rightLCA) return root;
+    else if(leftLCA && !rightLCA) return leftLCA;
+    else if(rightLCA && !leftLCA) return rightLCA;
+    else return NULL;
+}
+int main()
+{
+/*
+#ifndef ONLINE_JUDGE
+    freopen ("in.txt" , "r" , stdin);
+    freopen ("out.txt" , "w" , stdout);
+#endif
+*/
+
+	return 0;
+}
